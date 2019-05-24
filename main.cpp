f15166a0877300aada909c77b0e793f04a3c8e07
Erik Frojdh <erik.frojdh@gmail.com>
2019-05-24 18:40:08 +0200

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include "multiSlsDetector.h"
 #include <iostream>
 
-
-int main()
-{
-
+int main(){
     multiSlsDetector det;
     std::cout << "Detector type is: " << det.getDetectorTypeAsString() << '\n';
+    std::cout << "Hostname: " << det.getHostname() << "\n";
 
     det.setExposureTime(0.5, true);
     det.setNumberOfFrames(1);
     det.setExposurePeriod(0);
-    std::cout << "Hostname: " << det.getHostname() << "\n";
     std::cout << "exptime " << det.setExposureTime(-1, true) << " s\n";
 
     det.acquire();
